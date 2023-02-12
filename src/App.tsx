e47d122f9bf16b56b15e57cafd17e0423df4c667
olaoluwa vincent <olaoluwavincent@gmail.com>
2023-02-12 17:01:04 +0100

    chatbox merged

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,7 @@ import ChatBox from './components/ChatBox';
 type Props = {};
 
 const App = (props: Props) => {
-	return (
-		<div className='App'>
-			<ChatBox />
-		</div>
-	);
+	return <div className='App'></div>;
 };
 
 export default App;
