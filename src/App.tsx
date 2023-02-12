c6cdd0b5c23ad7ce4bf891b4e4326e087fe65fbc
olaoluwa vincent <olaoluwavincent@gmail.com>
2023-02-12 19:05:47 +0100

    cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,9 @@
 import React from 'react';
-import { Register } from './components';
 
 type Props = {};
 
 const App = (props: Props) => {
-	return (
-		<div className='App'>
-			<Register />
-		</div>
-	);
+	return <div className='App'></div>;
 };
 
 export default App;
