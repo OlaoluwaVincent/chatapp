808047807bf1ab8f087065d5b4705e83be6e8806
olaoluwa vincent <olaoluwavincent@gmail.com>
2023-02-14 05:50:27 +0100

    Chatlist header init

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,15 @@
 import { BrowserRouter } from 'react-router-dom';
-import { ToastContainer, toast } from 'react-toastify';
+import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Index from './pages/Index';
-import { Loading } from './components';
 
 type Props = {};
 
 const App = (props: Props) => {
 	return (
 		<BrowserRouter>
-			<div>
-				<Index />
-				<ToastContainer />
-			</div>
+			<Index />
+			<ToastContainer />
 		</BrowserRouter>
 	);
 };
